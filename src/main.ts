9374774ddb7d1894f90cdbe4ae70b21792281ebd
ing-reyes <ing.sistemas.reyes3@gmail.com>
2025-09-05 17:42:30 -0400

    add run mogodb

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
+import { MongoDB } from "./common/database/mongodb/mongodb-connect";
 import { AppServer } from "./server";
 
 
 function bootstrap(){
+    //* Run mongodb
+    new MongoDB().connect();
+
+    //* Run server
     const server = new AppServer();
 
     server.start();
